57bf85850c43a344fb9d636e515c39afb4cf70ec
Maksymilian Norkiewicz <maksymiliannorkiewicz@gmail.com>
2025-04-02 21:47:57 +0200

    New project structure, compatible with NEXT js

--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.chat),
-    path('chat', views.chat, name='chat'),
-    path('chat/<str:chat_id>/', views.chat, name='chat_with_id'),
-    path('answer/', views.answer, name="answer"),
-]
