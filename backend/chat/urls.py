49cda910b8336c4daf9eb7134479a32c3c8255be
Maksymilian Norkiewicz <maksymiliannorkiewicz@gmail.com>
2025-04-12 18:48:52 +0200

    '/' sign at the end of urls

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('chat', views.chat_view, name='chat'),
-    path('chat/redirect', views.chat_redirection_view, name='chat-redirect'),
+    path('chat/', views.chat_view, name='chat'),
+    path('chat/redirect/', views.chat_redirection_view, name='chat-redirect'),
     path('chat/<str:chat_id>/', views.chat_history_view, name='chat-history'),
 ]
