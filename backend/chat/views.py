990ba3cc787fddc51aaac76250f126d8817e3f01
Maksymilian Norkiewicz <maksymiliannorkiewicz@gmail.com>
2025-04-12 17:14:24 +0200

    Added asgiref library to views.py

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import StreamingHttpResponse
+from asgiref.sync import sync_to_async
 
 from .agent.chat_history import ChatHistory
 
