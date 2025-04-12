ce39f9048bf2456facc04ab6fcb9a3a515a0f5e2
Maksymilian Norkiewicz <maksymiliannorkiewicz@gmail.com>
2025-04-12 18:31:42 +0200

    Fixed view functions for chat

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,8 +1,9 @@
 # views.py
-from rest_framework.decorators import api_view, parser_classes
+from rest_framework.decorators import api_view, parser_classes, permission_classes
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from asgiref.sync import sync_to_async
 
 from .agent.chat_history import ChatHistory
@@ -11,22 +12,27 @@ chat_history = ChatHistory()
 
 
 @api_view(['GET'])
-def chat_history_view(request, chat_id=None):
-    if chat_id is not None:
-        if not chat_history.chat_exist(chat_id):
-            return Response({"error": "Chat doesn't exist."}, status=status.HTTP_404_NOT_FOUND)
-        
-        messages, title = chat_history.get_chat_history_for_html(chat_id)
-        data = {"chat_history": messages}
-        if title:
-            data["title"] = title
-        return Response(data, status=status.HTTP_200_OK)
-    else:
-        return Response({"error": "chat_id is required."}, status=status.HTTP_400_BAD_REQUEST)
+@permission_classes([IsAuthenticated])
+def chat_view(request):
+    return Response({}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def chat_history_view(request, chat_id):
+    if not chat_history.chat_exist(chat_id):
+        return Response({"error": "Chat doesn't exist."}, status=status.HTTP_404_NOT_FOUND)
+    
+    messages, title = chat_history.get_chat_history_for_html(chat_id)
+    data = {"chat_history": messages}
+    if title:
+        data["title"] = title
+    return Response(data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 @parser_classes([JSONParser])
+@permission_classes([IsAuthenticated])
 async def chat_redirection_view(request):
     try:
         question = request.data.get('question')
