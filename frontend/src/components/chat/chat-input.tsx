0c2ae599752a67cefcfb65a25c296ca633f8fabc
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2025-04-23 13:12:36 +0200

    feat loader chat input

--- frontend/src/components/chat/chat-input.tsx
+++ frontend/src/components/chat/chat-input.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ArrowUp } from 'lucide-react';
+import { ArrowUp, Loader } from 'lucide-react';
 import { useState } from 'react';
 import { Textarea } from '@/components/ui/textarea';
 import { cn } from '@/lib/utils';
@@ -9,23 +9,32 @@ import { Button } from '@/components/ui/button';
 
 function ChatInput() {
   const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(false);
   const { textareaRef, adjustHeight } = useAutoResizeTextarea({
     minHeight: 50,
     maxHeight: 200,
   });
 
   const handleSubmit = () => {
+    if (loading) return;
+    setLoading(true);
+
     setInputValue('');
     adjustHeight(true);
+
+    // Simulate a network request
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
   };
 
   return (
-    <div className="relative mx-auto w-full min-w-sm">
+    <div className="relative mx-auto w-full max-w-xl">
       <Textarea
         ref={textareaRef}
         id="chat-main-input"
         placeholder="Zapytaj mnie o coś"
-        className="min-h-[56px] w-full resize-none rounded-3xl border-none py-4 pr-12 pl-6 leading-[1.2] text-wrap"
+        className="min-h-[56px] w-full resize-none rounded-md border-none py-4 pr-12 pl-6 leading-[1.2] text-wrap"
         value={inputValue}
         onKeyDown={e => {
           if (e.key === 'Enter') {
@@ -42,14 +51,20 @@ function ChatInput() {
       <Button
         variant="ghost"
         size="icon"
-        className="absolute top-1/2 right-3 -translate-y-1/2 rounded-xl"
+        className="absolute top-1/2 right-3 -translate-y-1/2 rounded-full"
+        disabled={loading}
+        onClick={handleSubmit}
       >
-        <ArrowUp
-          className={cn(
-            'size-4 transition-opacity',
-            inputValue ? 'opacity-100' : 'opacity-30',
-          )}
-        />
+        {loading ? (
+          <Loader className="animate-spin" />
+        ) : (
+          <ArrowUp
+            className={cn(
+              'transition-opacity',
+              inputValue ? 'opacity-100' : 'opacity-30',
+            )}
+          />
+        )}
       </Button>
     </div>
   );
