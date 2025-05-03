04164d559e497d557af4008ae92de2ba7094fe27
Maksymilian Norkiewicz <maksymiliannorkiewicz@gmail.com>
2025-05-03 19:59:07 +0200

    Question parameter visible in swagger

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -4,7 +4,10 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework import status, serializers
 from rest_framework.permissions import IsAuthenticated
-from asgiref.sync import sync_to_async
+from rest_framework import mixins, viewsets
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from asgiref.sync import async_to_sync
 
 from .agent.chat_history import ChatHistory
 
@@ -19,6 +22,19 @@ class ChatRedirectionSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         return instance
+
+
+class ChatRedirectionView(
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ChatRedirectionSerializer
+    queryset = []
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -43,10 +59,18 @@ def chat_history_view(request, chat_id):
     return Response(data, status=status.HTTP_200_OK)
 
 
+@swagger_auto_schema(
+    method='post',
+    request_body=ChatRedirectionSerializer,
+    responses={201: openapi.Response('Redirect', schema=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        properties={'redirect_url': openapi.Schema(type=openapi.TYPE_STRING)}
+    ))}
+)
 @api_view(['POST'])
 @parser_classes([JSONParser])
 @permission_classes([IsAuthenticated])
-async def chat_redirection_view(request):
+def chat_redirection_view(request):
     try:
         serializer = ChatRedirectionSerializer(data=request.data)
         if not serializer.is_valid():
@@ -60,7 +84,7 @@ async def chat_redirection_view(request):
                             status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            chat_id = await chat_history.create_new_chat(user_email, question)
+            chat_id = async_to_sync(chat_history.create_new_chat)(user_email, question)
         except Exception:
             return Response({"error": "Could not create chat."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
