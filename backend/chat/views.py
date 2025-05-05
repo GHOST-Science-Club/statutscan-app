145be844ff290da0168999e4cee0ad6e6a4b84f1
Maksymilian Norkiewicz <maksymiliannorkiewicz@gmail.com>
2025-05-05 16:50:43 +0200

    synchronous create_new_chat function

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -92,8 +92,9 @@ def chat_redirection_view(request):
             )
 
         try:
-            chat_id = async_to_sync(chat_history.create_new_chat)(user_email, question)
-        except Exception:
+            chat_id = chat_history.create_new_chat(user_email, question)
+        except Exception as e:
+            print(f"Error: {e}")
             return Response({"error": "Could not create chat."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         return Response({
