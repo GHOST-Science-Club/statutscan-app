d8f2b4119aad2b6ff3aba9ab9e8dcfded17a2afd
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2025-05-06 14:20:52 +0200

    feat: redirect in getChatFirstMessage

--- frontend/src/actions/getChatFirstMsg.ts
+++ frontend/src/actions/getChatFirstMsg.ts
@@ -2,11 +2,12 @@
 
 import { fetchBackend } from '@/lib/fetchBackend';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 async function getChatFirstMsg({ question }: { question: string }) {
   const cookieStore = await cookies();
   const access = cookieStore.get('access')?.value;
-  console.log(question);
+
   const res = await fetchBackend({
     url: '/chat/redirect/',
     method: 'POST',
@@ -16,9 +17,10 @@ async function getChatFirstMsg({ question }: { question: string }) {
     },
   });
 
-  console.log(res);
   const json = await res.json();
-  console.log(json);
+  if (json.redirect_url) {
+    redirect(json.redirect_url);
+  }
 }
 
 export { getChatFirstMsg };
