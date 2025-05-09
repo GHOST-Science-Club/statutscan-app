ea2f0a6a52caef4c27f9172db8937da9ecec442c
YIlyaA <anukovich@gmail.com>
2025-05-09 21:47:26 +0200

    fix: enable auto file reload in Docker and channels auth + fixes chat_history

--- backend/statutscan_project/asgi.py
+++ backend/statutscan_project/asgi.py
@@ -1,19 +1,26 @@
 import os
+from django.conf import settings
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'statutscan_project.settings')
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statutscan_project.settings")
 django_asgi_app = get_asgi_application()
 
+from chat.middleware import JWTAuthMiddleware
 from chat import routing
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
-    ),
-})
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            OriginValidator(
+                JWTAuthMiddleware(URLRouter(routing.websocket_urlpatterns)),
+                settings.CORS_ALLOWED_ORIGINS,
+            )
+        ),
+    }
+)
