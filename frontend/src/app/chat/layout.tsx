3014d7eecec7c1c4ff4cc8fe57884de2939499e7
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2025-05-16 17:13:37 +0200

    feat: sidebar improvements

--- frontend/src/app/chat/layout.tsx
+++ frontend/src/app/chat/layout.tsx
@@ -1,48 +1,9 @@
 import { CSSProperties, ReactNode } from 'react';
-import { ArrowUpRight, Plus } from 'lucide-react';
+import { Plus } from 'lucide-react';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { ChatSidebar } from '@/components/chat/chat-sidebar';
 import { Button } from '@/components/ui/button';
-
-const sidebarItems = [
-  {
-    label: 'Lorem ipsum',
-    content: [
-      'Lorem ipsum dolor sit amet',
-      'Lorem ipsum dolor',
-      'Lorem ipsum dolor sit amet consectetur...',
-    ],
-  },
-  {
-    label: 'Lorem ipsum',
-    content: [
-      'Lorem ipsum dolor sit amet consectetur...',
-      'Lorem ipsum dolor sit',
-    ],
-  },
-  {
-    label: 'Lorem ipsum',
-    content: [
-      'Lorem ipsum dolor',
-      'Lorem ipsum dolor sit amet consectetur...',
-      'Lorem ipsum dolor sit amet consectetur...',
-      'Lorem ipsum dolor sit amet',
-    ],
-  },
-  {
-    label: 'Lorem ipsum',
-    content: [
-      'Lorem ipsum dolor sit',
-      'Lorem ipsum dolor sit amet consectetur...',
-      'Lorem ipsum',
-      'Lorem ipsum dolor sit amet consectetur...',
-      'Lorem ipsum dolor sit amet consectetur...',
-      'Lorem ipsum dolor',
-      'Lorem ipsum dolor sit amet consectetur...',
-      'Lorem ipsum dolor sit',
-    ],
-  },
-];
+import Link from 'next/link';
 
 export default function ChatLayout({ children }: { children: ReactNode }) {
   return (
@@ -55,18 +16,16 @@ export default function ChatLayout({ children }: { children: ReactNode }) {
         } as CSSProperties
       }
     >
-      <ChatSidebar items={sidebarItems} />
+      <ChatSidebar />
       <div className="absolute m-2">
         <SidebarTrigger className="size-9" />
-        <Button aria-label="Nowy czat" variant="ghost" size="icon">
-          <Plus />
+        <Button aria-label="Nowy czat" variant="ghost" size="icon" asChild>
+          <Link href="/chat">
+            <Plus />
+          </Link>
         </Button>
       </div>
       {children}
-      <Button variant="primary" className="absolute right-0 m-2">
-        Lorem ipsum
-        <ArrowUpRight />
-      </Button>
     </SidebarProvider>
   );
 }
--- frontend/src/components/chat/chat-sidebar.tsx
+++ frontend/src/components/chat/chat-sidebar.tsx
@@ -13,15 +13,10 @@ import { CircleHelp, Plus } from 'lucide-react';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Logo } from '@/components/ui/logo';
 import Link from 'next/link';
+import { getChats } from '@/lib/chat/getChats';
 
-type Props = {
-  items: {
-    label: string;
-    content: string[];
-  }[];
-};
-
-export function ChatSidebar({ items }: Props) {
+export async function ChatSidebar() {
+  const chats = await getChats();
   return (
     <Sidebar collapsible="offcanvas">
       <SidebarHeader className="flex flex-row items-end justify-end pl-4">
@@ -30,22 +25,34 @@ export function ChatSidebar({ items }: Props) {
         </Link>
 
         <SidebarTrigger className="size-9" />
-        <Button aria-label="Nowy czat" variant="ghost" size="icon">
-          <Plus />
+        <Button aria-label="Nowy czat" variant="ghost" size="icon" asChild>
+          <Link href="/chat">
+            <Plus />
+          </Link>
         </Button>
       </SidebarHeader>
       <SidebarContent className="overflow-hidden">
         <ScrollArea className="h-full">
-          {items.map((item, i) => (
-            <SidebarGroup key={i} className="px-4">
-              <SidebarGroupLabel>{item.label}</SidebarGroupLabel>
+          {chats.length === 0 ? (
+            <p className="py-5 text-center text-sm">
+              Nie utworzono żadnych czatów
+            </p>
+          ) : (
+            <SidebarGroup className="px-4">
+              <SidebarGroupLabel>Wszystkie czaty</SidebarGroupLabel>
               <SidebarGroupContent className="flex flex-col gap-2 text-ellipsis">
-                {item.content.map((item, j) => (
-                  <p key={j}>{item}</p>
+                {chats.map((chat, i) => (
+                  <Link
+                    key={i}
+                    href={`/chat/${chat.id}`}
+                    className="text-ellipsis"
+                  >
+                    {chat.label}
+                  </Link>
                 ))}
               </SidebarGroupContent>
             </SidebarGroup>
-          ))}
+          )}
         </ScrollArea>
       </SidebarContent>
       <SidebarFooter>
