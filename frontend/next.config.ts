f1cdb0ac5575bd6fc01ee3371b4e5f59e41baf4f
Illia Yanukovich <anukovich2@gmail.com>
2025-05-21 16:31:00 +0200

    fix: fixed windows issue - auto changes applying to docker (multiplatform)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,6 +7,16 @@ const nextConfig: NextConfig = {
   },
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   output: 'standalone',
+  webpack: (config, { dev }) => {  // ONLY FOR DEV!!!!!!!!!!
+    if (dev) {
+      config.watchOptions = {
+        ignored: ['**/node_modules', '**/.next'],
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
 };
 
 const withMDX = createMDX({});
