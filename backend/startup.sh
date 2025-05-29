6517dbc934e175dd15635745b1d72bf37592d84b
Maksymilian Norkiewicz <maksymiliannorkiewicz@gmail.com>
2025-05-29 15:25:12 +0200

    startup file moved to root

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Migrating changes to database"
 python3 manage.py migrate --noinput
+
+echo "Collecting static files"
 python3 manage.py collectstatic --noinput
+
+echo "Running application"
 uvicorn statutscan_project.asgi:application --host 0.0.0.0 --port 8000 --reload
