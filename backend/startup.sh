3adf10ffa8bfaab901ab1b3443b4cc47bd0d6fa7
Maksymilian Norkiewicz <maksymiliannorkiewicz@gmail.com>
2025-06-03 15:59:03 +0200

    Reverting a mistaken merge into main

---
+++ backend/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -f manage.py ]; then
+  cd backend
+fi
+
+echo "Migrating changes to database"
+python3 manage.py migrate --noinput
+
+echo "Collecting static files"
+python3 manage.py collectstatic --noinput
+
+echo "Running application"
+uvicorn statutscan_project.asgi:application --host 0.0.0.0 --port 8000 --reload
