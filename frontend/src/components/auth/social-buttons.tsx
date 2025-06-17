55cc6eb68086bf00758c2cf023ae56ad290c9c14
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2025-06-17 15:48:25 +0200

    feat: ws url access token

--- frontend/src/components/auth/social-buttons.tsx
+++ frontend/src/components/auth/social-buttons.tsx
@@ -1,14 +1,11 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { Icons } from '@/components/ui/icons';
-import { handleGoogleSignIn } from '@/lib/auth/googleSignIn';
-
 function SocialButtons() {
-  return (
-    <Button variant="secondary" className="w-full" onClick={handleGoogleSignIn}>
-      <Icons icon="google" />
-      Kontynuuj z Google
-    </Button>
-  );
+  return <></>;
+  // TODO - to implement
+  // return (
+  //   <Button variant="secondary" className="w-full" onClick={handleGoogleSignIn}>
+  //     <Icons icon="google" />
+  //     Kontynuuj z Google
+  //   </Button>
+  // );
 }
 export { SocialButtons };
